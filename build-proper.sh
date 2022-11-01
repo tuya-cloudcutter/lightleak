e3ee4402070dd975c813b691f8394e00523b47cc
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2022-11-01 19:50:17 +0100

    Use common source code for T/N proper

--- build-proper.sh
+++ build-proper.sh
@@ -5,10 +5,11 @@ mkdir -p build
 
 flags="-mthumb -mcpu=arm968e-s -march=armv5te -nostartfiles -Wl,-Ttext,0x120000 -Wl,--entry,main -std=c99 -Wno-builtin-declaration-mismatch -Os -g"
 
-for dir in payload-proper/*; do
-	name=${dir##*/}
-	arm-none-eabi-gcc ${flags} ${dir}/*.c -I${dir} -o build/${name}.o
-	arm-none-eabi-objcopy -O binary build/${name}.o build/${name}.bin
-	arm-none-eabi-objdump -dS build/${name}.o > site/${name}.lst
-	ltchiptool soc bkpackager encrypt 510fb093a3cbeadc5993a17ec7adeb03 build/${name}.bin site/${name}.bin 0x120000 -c
-done
+SOURCE=$1
+OUTPUT=$2
+DEFINE=$3
+
+arm-none-eabi-gcc ${flags} payload-proper/${SOURCE}/*.c -D${DEFINE} -Ipayload-proper/${SOURCE} -o build/${OUTPUT}.o
+arm-none-eabi-objcopy -O binary build/${OUTPUT}.o build/${OUTPUT}.bin
+arm-none-eabi-objdump -dS build/${OUTPUT}.o > site/${OUTPUT}.lst
+ltchiptool soc bkpackager encrypt 510fb093a3cbeadc5993a17ec7adeb03 build/${OUTPUT}.bin site/${OUTPUT}.bin 0x120000 -c
